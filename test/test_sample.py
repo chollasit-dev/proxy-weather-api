f914699969e395fbbe428710c71a3d9390371e9d
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2024-07-21 21:51:18 +0700

    test(sample): add test for '/sample/test_escaping_script'
    write unit test to validate if worked as expected:
    
    - return text as-if passed in url when passing falsy value to escaping parameter
    - escaped text before returning when passing truthy value

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,6 +1,8 @@
+import pytest
 from flask import Response
+from markupsafe import escape
 
-from app.sample.routes import index
+from app.sample.routes import index, try_escaping_script
 from test import app
 
 
@@ -8,3 +10,27 @@ def test_index() -> None:
     with app.test_request_context():
         response: Response = index()
         assert response.status_code == 200
+
+
+test_script: str = "<script>alert('test')</script>"
+
+
+@pytest.mark.parametrize(
+    "script, escaping, expected",
+    [
+        (test_script, "True", escape(test_script)),
+        (test_script, "true", escape(test_script)),
+        (test_script, "yes", escape(test_script)),
+        (test_script, "1", escape(test_script)),
+        (test_script, "False", test_script),
+        (test_script, "false", test_script),
+        (test_script, "no", test_script),
+        (test_script, "0", test_script),
+    ],
+)
+def test_try_escaping_script(script: str, escaping: str, expected: str) -> None:
+    with app.test_request_context(
+        query_string={"script": script, "escaping": escaping}
+    ):
+        response: Response = try_escaping_script()
+        assert response.get_data(as_text=True) == expected
