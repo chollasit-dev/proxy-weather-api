b8efa570b62e2e989c3fe4d4edc3eba4c1eb4ac5
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2024-07-21 21:42:39 +0700

    feat(sample): add route to test escaping input text
    add route '/try_escaping_script' to test how flask handle user request input

--- app/sample/routes.py
+++ app/sample/routes.py
@@ -1,4 +1,5 @@
-from flask import Response, make_response
+from flask import Response, abort, make_response, request, url_for
+from markupsafe import escape
 
 from app.sample import bp
 
@@ -9,3 +10,21 @@ def index() -> Response:
     To get actual info, please send POST request to "/api/..." instead.
     """
     return make_response(content, 200)
+
+
+@bp.get("/try_escaping_script")
+def try_escaping_script() -> Response:
+    script: str | None = request.args.get("script", None)
+    if script is None:
+        content: str = f"""No script parameter provide!
+        To pass arguments, simply type arguments in the address bar
+        "{url_for('try_escaping_script',
+                  script='<script>any_script_here()</script>',
+                  escaping='true_or_false')}"
+        """
+        return abort(400, content)
+
+    escaping: str = request.args.get("escaping", "true")
+    is_escaping: bool = False if escaping.lower() in ("false", "no", "0") else True
+
+    return make_response(escape(script) if is_escaping else script, 200)
